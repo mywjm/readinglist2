d4586bc8149d11d781e3af2b19aebb4a8dcf4785
Yorick <18814129853@163.com>
2017-08-25 18:43:32 +0800

    web test

--- src/test/java/com/example/readinglist2/MongoTests.java
+++ src/test/java/com/example/readinglist2/MongoTests.java
@@ -1,25 +1,52 @@
 package com.example.readinglist2;
 
+import org.apache.bcel.generic.BREAKPOINT;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.sql.Driver;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Fail.fail;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(
+        classes = ReadingListApplication.class
+)
+@WebIntegrationTest(randomPort=true)
 public class MongoTests {
+    //配置浏览器驱动
+    private static InternetExplorerDriver browser;
+    @Value("${local.server.port}")
+    private int port;
 
     @Autowired
     private ReadingListRepository bookRepository;
@@ -27,6 +54,15 @@ public class MongoTests {
     @Autowired
     private WebApplicationContext webApplicationContext;
     private MockMvc mockMvc;
+    @BeforeClass
+    public static void openBrowser() {
+        browser = new InternetExplorerDriver();
+        browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    @AfterClass
+    public static void closeBrowser() {
+        browser.quit();
+    }
     @Before
     public void setupMockMvc() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
@@ -90,4 +126,37 @@ public class MongoTests {
                 .andExpect(model().attribute("books", hasSize(4)))
                 .andExpect(model().attribute("books", contains(samePropertyValuesAs(expectedBook))));
     }
+
+    @Test(expected = HttpClientErrorException.class)
+    public void pageNotFound() {
+        try {
+            RestTemplate rest = new RestTemplate();
+            rest.getForObject("http://localhost:8080/bogusPage", String.class);
+            fail("Should result in http 404");
+
+        }catch (HttpClientErrorException e) {
+            assertEquals(HttpStatus.NOT_FOUND, e.getStatusCode());
+            throw e;
+        }
+    }
+
+    @Test
+    public void addBookToEmptyList() {
+        String baseUrl = "http://localhost:"+port;
+        browser.get(baseUrl);
+        assertEquals("You have no books in your book list", browser.findElementByTagName("div").getText());
+        browser.findElementByName("title").sendKeys("BOOK TITLE");
+        browser.findElementByName("author").sendKeys("BOOK AUTHOR");
+        browser.findElementByName("isbn").sendKeys("1234567890");
+        browser.findElementByName("description").sendKeys("DESCRIPTION");
+        browser.findElementByName("form").submit();
+
+        WebElement dl = browser.findElementByCssSelector("dt.bookHeadline");
+        assertEquals("BOOK TITLE BY BOOK AUTHOR (ISBN: 1234567890)", dl.getText());
+
+        WebElement dt = browser.findElementByCssSelector("dd.bookDescription");
+        assertEquals("DESCRIPTION", dt.getText());
+    }
+
+
 }
